42f118f3793263c3dd585bc6dee06b034bbed106
uvinduP <65161616+Uvindu-bit@users.noreply.github.com>
2021-02-15 11:29:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+ask = input("What do you want to convert? (Ex: lkr to usd or anything like this)") 
+
+if ask == "lkr to usd":
+    print("how much lkr?")
+    ask1 = int(input("print"))
+    b = ask1 * 193
+    print(b)
+
+elif ask == "lkr to euro":
+    print("how much lkr?")
+    ask1 = int(input("print"))
+    b = ask1 * 235
+    print(b)
+
+elif ask == "lkr to singapordollar":
+    print("how much lkr?")
+    ask1 = int(input("print"))
+    b = ask1 * 146
+    print(b)
